188bc2b6f2edca83f17d20d015c027e97c428b69
stardust <88991140+stardustgd@users.noreply.github.com>
2024-05-24 12:41:07 -0700

    fix nvim-ts-autotag

--- lua/plugins/nvim-ts-autotag.lua
+++ lua/plugins/nvim-ts-autotag.lua
@@ -2,7 +2,15 @@ return {
   {
     "windwp/nvim-ts-autotag",
     dependencies = "nvim-treesitter/nvim-treesitter",
-    config = function() require("nvim-ts-autotag").setup {} end,
+    config = function()
+      require("nvim-ts-autotag").setup {
+        opts = {
+          enable_close = true,
+          enable_rename = true,
+          enable_close_on_slash = false,
+        },
+      }
+    end,
     lazy = true,
     event = "VeryLazy",
   },
